a1687ff4ee65b4f37544590ece0bce8d2ef88545
saroko-dnd <isaroko@mail.ru>
2018-02-21 20:08:57 +0200

    Class Sorter methods were successfully implemented

--- src/index.js
+++ src/index.js
@@ -1,31 +1,47 @@
 class Sorter {
-  constructor() {
-    // your implementation
-  }
+    constructor() {
+        this._elements = [];
+        this._comparator = null;
+    }
 
-  add(element) {
-    // your implementation
-  }
+    add(element) {
+        this._elements.push(element);
+    }
 
-  at(index) {
-    // your implementation
-  }
+    at(index) {
+        return this._elements[index];
+    }
 
-  get length() {
-    // your implementation
-  }
+    get length() {
+        return this._elements.length;
+    }
 
-  toArray() {
-    // your implementation
-  }
+    toArray() {
+        return this._elements;
+    }
 
-  sort(indices) {
-    // your implementation
-  }
+    sort(indices) {
+        const arrayForSort = [];
+        const sortedIndices = [...indices].sort(this._defaultComparator);
 
-  setComparator(compareFunction) {
-    // your implementation
-  }
+        sortedIndices.forEach(index =>
+            arrayForSort.push(this._elements[index])
+        );
+
+        arrayForSort.sort(this._comparator || this._defaultComparator);
+
+        arrayForSort.forEach(
+            (element, index) => (this._elements[sortedIndices[index]] = element)
+        );
+    }
+
+    setComparator(compareFunction) {
+        this._comparator = compareFunction;
+    }
+
+    _defaultComparator(a, b) {
+        return a - b;
+    }
 }
 
 module.exports = Sorter;
